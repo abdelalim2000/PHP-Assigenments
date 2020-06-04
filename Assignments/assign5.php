ef83c1ed3a25d0bb0f13af5d69ed470e6037fa95
Mohamed <mohamed.abdelalilm2000@gmail.com>
2020-06-04 16:37:19 +0200

    changing in assign 5

--- Assignments/assign5.php
+++ Assignments/assign5.php
@@ -15,7 +15,33 @@ function _pyramid($row)
 
 _pyramid(10);
 
+//factorial
 
+function _fact($fact)
+{
+    $result = 1;
+    for ($i = 1; $i <= $fact; $i++) {
+        $result = $result * $i;
+    }
+    echo "fact {$fact} is {$result}";
+}
+
+_fact(5);
+
+//power of number
+echo "<br>";
+
+
+function _pow($num, $power)
+{
+    $result = 1;
+    for ($i = 1; $i <= $power; $i++) {
+        $result = $result * $num;
+    }
+    echo "{$result}";
+}
+
+_pow(5, 2);
 //check if prime number with function
 
 function _primeNumber($num)
